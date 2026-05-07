29aeb84e227be64cb07fe789aa46bf08742d73f6
flayregp-blip <flayregp@gmail.com>
2026-05-08 01:00:29 +0300

    const_res.dart

--- lib/utilities/const_res.dart
+++ lib/utilities/const_res.dart
@@ -1,5 +1,5 @@
 const String baseURL = 'https://fistayl.com/';
-const String apiURL = '${baseURL}api';
+const String apiURL = '${baseURL}api/'
 const String apiKey = 'retry123';
 
 // If you change this topic you also change backend .env file
