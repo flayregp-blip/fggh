ac3c064ef4679b8bd9cae8931c1df9b404eaa7d5
flayregp-blip <flayregp@gmail.com>
2026-05-07 21:06:16 +0300

    const_res.dart

--- lib/utilities/const_res.dart
+++ lib/utilities/const_res.dart
@@ -1,9 +1,9 @@
-const String baseURL = 'https://fistayl.com/public/';
-const String apiURL = '${baseURL}api/';
+const String baseURL = 'https://fistayl.com/';
+const String apiURL = '${baseURL}api';
 const String apiKey = 'retry123';
 
 // If you change this topic you also change backend .env file
 String notificationTopic = "flayr";
 
-String revenueCatAndroidApiKey = "goog_GyjhfxgHEABgtZzglfKTYoyQuUg";
-String revenueCatAppleApiKey = "________";
+String revenueCatAndroidApiKey = "goog_GyjhfxgHEABgtZzg1fKTYoyQuUg";
+String revenueCatAppleApiKey = "_______";
