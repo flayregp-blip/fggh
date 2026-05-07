d88d16ce3e44ce4eb1a2da367265029c8c27b532
flayregp-blip <flayregp@gmail.com>
2026-05-07 11:17:25 +0300

    send_gift_dialog.dart

--- lib/screen/gift_sheet/send_gift_dialog.dart
+++ lib/screen/gift_sheet/send_gift_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:figma_squircle_updated/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:shortzz/common/extensions/string_extension.dart';
 import 'package:shortzz/common/widget/custom_image.dart';
 import 'package:shortzz/common/widget/gradient_text.dart';
@@ -32,31 +33,31 @@ class _SendGiftDialogState extends State<SendGiftDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      backgroundColor: whitePure(context),
-      shape: RoundedRectangleBorder(borderRadius: SmoothBorderRadius(cornerRadius: 20)),
+      backgroundColor: Colors.transparent,
+      elevation: 0,
       alignment: const Alignment(0, 0.4),
-      child: AspectRatio(
-        aspectRatio: 1.8,
-        child: Container(
-          decoration: ShapeDecoration(
-              shape: SmoothRectangleBorder(borderRadius: SmoothBorderRadius(cornerRadius: 20)),
-              color: whitePure(context)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomImage(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Lottie.network(
+            widget.gift.image?.addBaseURL() ?? '',
+            width: 200,
+            height: 200,
+            fit: BoxFit.contain,
+            errorBuilder: (context, error, stackTrace) {
+              return CustomImage(
                   image: widget.gift.image?.addBaseURL(),
-                  size: const Size(90, 90),
-                  radius: 0),
-              Text(LKey.yourGiftHasBeenSent.tr,
-                  style: TextStyleCustom.outFitRegular400(
-                      fontSize: 15, color: textLightGrey(context))),
-              GradientText(LKey.successfully.tr,
-                  gradient: StyleRes.themeGradient,
-                  style: TextStyleCustom.unboundedSemiBold600(fontSize: 15)),
-            ],
+                  size: const Size(150, 150),
+                  radius: 0);
+            },
           ),
-        ),
+          Text(LKey.yourGiftHasBeenSent.tr,
+              style: TextStyleCustom.outFitRegular400(
+                  fontSize: 15, color: whitePure(context))),
+          GradientText(LKey.successfully.tr,
+              gradient: StyleRes.themeGradient,
+              style: TextStyleCustom.unboundedSemiBold600(fontSize: 15)),
+        ],
       ),
     );
   }
