6b12045b7b5f37a49b15677ab09f5402584420f8
flayregp-blip <flayregp@gmail.com>
2026-05-15 12:22:51 +0300

    color_res.dart

--- lib/utilities/color_res.dart
+++ lib/utilities/color_res.dart
@@ -1,52 +1,36 @@
 hereimport 'package:flutter/material.dart';
 
 class ColorRes {
-
-  static const Color blackPure = Color(0xFF05060A);
-  static const Color whitePure = Color(0xFFFFFFFF);
-
-  static const Color bgDeep = Color(0xFFF6F8FF);
-
-  static const Color themeColor = Color(0xFFFFFFFF);
-
-  static const Color bgLightGrey = Color(0xFFF4F7FF);
-  static const Color bgGrey = Color(0xFFEFF3FF);
-  static const Color bgMediumGrey = Color(0xFFE4EAFF);
-
-  static const Color themeGradient1 = Color(0xFF6D5BFF);
-  static const Color themeGradient2 = Color(0xFF3EC5FF);
-
-  static const Color themeAccentSolid = Color(0xFFEAEFFF);
-
-  static const Color borderSubtle = Color(0xFFE6EBFF);
-  static const Color borderMedium = Color(0xFFD5DDFF);
-
-  static const Color textPrimary = Color(0xFF111827);
-  static const Color textSecondary = Color(0xFF667085);
-  static const Color textMuted = Color(0xFF98A2B3);
-
-  static const Color textDarkGrey = Color(0xFF1F2937);
-  static const Color textLightGrey = Color(0xFF94A3B8);
-
-  static const Color orange = Color(0xFF6D5BFF);
-  static const Color orangeLight = Color(0xFF9A8CFF);
-  static const Color orangeDim = Color(0x226D5BFF);
-
-  static const Color blueFollow = Color(0xFF00B8FF);
-  static const Color blueLight = Color(0xFF6ED8FF);
-  static const Color blueDim = Color(0x2200B8FF);
-
-  static const Color battleProgressColor = Color(0xFFFF4FD8);
-  static const Color battleDim = Color(0x22FF4FD8);
-
-  static const Color green = Color(0xFF12D18E);
-  static const Color green1 = Color(0xFF55F5B5);
-  static const Color greenDim = Color(0x2212D18E);
-
-  static const Color likeRed = Color(0xFFFF2E63);
-  static const Color redDim = Color(0x22FF2E63);
-
-  static const Color textStoryBgGradient2 = Color(0xFFFF3CAC);
-
-  static const Color disabledGrey = Color(0xFFC5CCE0);
+  static const Color blackPure      = Color(0xFF000000);
+  static const Color whitePure      = Color(0xFFFFFFFF);
+  static const Color bgDeep         = Color(0xFF0D0E12);
+  static const Color themeColor     = Color(0xFF15161A);
+  static const Color themeGradient2 = Color(0xFF1C1D22);
+  static const Color themeGradient1 = Color(0xFF242529);
+  static const Color themeAccentSolid = Color(0xFF2E2F35);
+  static const Color borderSubtle   = Color(0xFF2A2B30);
+  static const Color borderMedium   = Color(0xFF3A3B42);
+  static const Color textPrimary    = Color(0xFFF2F2F4);
+  static const Color textSecondary  = Color(0xFF9A9AA8);
+  static const Color textMuted      = Color(0xFF5A5A68);
+  static const Color textDarkGrey   = Color(0xFF454550);
+  static const Color textLightGrey  = Color(0xFF8B8B98);
+  static const Color orange         = Color(0xFFFF7A19);
+  static const Color orangeLight    = Color(0xFFFF9A4D);
+  static const Color orangeDim      = Color(0x33FF7A19);
+  static const Color blueFollow     = Color(0xFF3E8BFF);
+  static const Color blueLight      = Color(0xFF6AABFF);
+  static const Color blueDim        = Color(0x263E8BFF);
+  static const Color battleProgressColor = Color(0xFF2CC3FF);
+  static const Color battleDim      = Color(0x262CC3FF);
+  static const Color green          = Color(0xFF009821);
+  static const Color green1         = Color(0xFF34D948);
+  static const Color greenDim       = Color(0x2634D948);
+  static const Color likeRed        = Color(0xFFFF5751);
+  static const Color textStoryBgGradient2 = Color(0xFFFF5757);
+  static const Color redDim         = Color(0x26FF5751);
+  static const Color bgLightGrey    = Color(0xFFF9F9F9);
+  static const Color bgGrey         = Color(0xFFEEEEEE);
+  static const Color bgMediumGrey   = Color(0xFFF4F4F4);
+  static const Color disabledGrey   = Color(0xFFBCBCC8);
 }
