f9bdd6a45360551a78c4c700d9e2c2dbbb46b09e
flayregp-blip <flayregp@gmail.com>
2026-06-06 23:39:07 +0300

    Centralize Supabase and OneSignal configuration keys in const_res.dart

--- lib/utilities/const_res.dart
+++ lib/utilities/const_res.dart
@@ -7,3 +7,10 @@ String notificationTopic = "flayr";
 
 String revenueCatAndroidApiKey = "goog_GyjhfxgHEABgtZzg1fKTYoyQuUg";
 String revenueCatAppleApiKey = "_______";
+
+// Supabase
+const String supabaseUrl = 'https://jzausyutomjmvghiuogw.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6YXVzeXV0b21qbXZnaGl1b2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwODUzNDgsImV4cCI6MjA5NDY2MTM0OH0.YvmP8PGiANmkCtennitwYWHwroK9fLwyiU6WDMVwYL0';
+
+// OneSignal
+const String oneSignalAppId = '1244b2fa-31cd-4bf2-a067-10b9246d659d';
