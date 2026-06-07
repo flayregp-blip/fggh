9d5de5c534b235a70775ad8c9fe48f75931f1a29
flayregp-blip <flayregp@gmail.com>
2026-06-07 13:27:14 +0300

    Provide clean theme_res.dart with all helper functions

--- lib/utilities/theme_res.dart
+++ lib/utilities/theme_res.dart
@@ -6,9 +6,7 @@ class ThemeRes {
   static ThemeData lightTheme(BuildContext context) {
     return ThemeData(
       scaffoldBackgroundColor: ColorRes.whitePure,
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        backgroundColor: ColorRes.whitePure,
-      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: ColorRes.whitePure),
       appBarTheme: const AppBarTheme(backgroundColor: ColorRes.bgLightGrey),
       fontFamily: FontRes.outFitRegular400,
       bottomSheetTheme: const BottomSheetThemeData(backgroundColor: ColorRes.whitePure),
@@ -37,50 +35,16 @@ class ThemeRes {
   }
 }
 
-// Helper functions
-Color whitePure(BuildContext context) {
-  return Theme.of(context).textTheme.titleLarge?.color ?? ColorRes.whitePure;
-}
-
-Color textDarkGrey(BuildContext context) {
-  return Theme.of(context).textTheme.titleMedium?.color ?? ColorRes.textDarkGrey;
-}
-
-Color textLightGrey(BuildContext context) {
-  return Theme.of(context).textTheme.titleSmall?.color ?? ColorRes.textLightGrey;
-}
-
-Color bgGrey(BuildContext context) {
-  return Theme.of(context).dividerColor ?? ColorRes.bgGrey;
-}
-
-Color themeAccentSolid(BuildContext context) {
-  return Theme.of(context).textTheme.labelSmall?.color ?? ColorRes.themeAccentSolid;
-}
-
-Color disableGrey(BuildContext context) {
-  return Theme.of(context).textTheme.labelLarge?.color ?? ColorRes.disabledGrey;
-}
-
-Color scaffoldBackgroundColor(BuildContext context) {
-  return Theme.of(context).scaffoldBackgroundColor ?? ColorRes.whitePure;
-}
-
-Color blueFollow(BuildContext context) {
-  return Theme.of(context).cardTheme.color ?? ColorRes.blueFollow;
-}
-
-Color bgMediumGrey(BuildContext context) {
-  return Theme.of(context).cardColor ?? ColorRes.bgMediumGrey;
-}
-
-Color blackPure(BuildContext context) {
-  return Theme.of(context).primaryColorDark ?? ColorRes.blackPure;
-}
-
-Color bgLightGrey(BuildContext context) {
-  return Theme.of(context).appBarTheme.backgroundColor ?? ColorRes.bgLightGrey;
-
-Color themeColor(BuildContext context) {
-  return Theme.of(context).canvasColor ?? ColorRes.themeColor;
-}
+// Helper functions - all properly defined
+Color whitePure(BuildContext context) => Theme.of(context).textTheme.titleLarge?.color ?? ColorRes.whitePure;
+Color textDarkGrey(BuildContext context) => Theme.of(context).textTheme.titleMedium?.color ?? ColorRes.textDarkGrey;
+Color textLightGrey(BuildContext context) => Theme.of(context).textTheme.titleSmall?.color ?? ColorRes.textLightGrey;
+Color bgGrey(BuildContext context) => Theme.of(context).dividerColor ?? ColorRes.bgGrey;
+Color themeAccentSolid(BuildContext context) => Theme.of(context).textTheme.labelSmall?.color ?? ColorRes.themeAccentSolid;
+Color disableGrey(BuildContext context) => Theme.of(context).textTheme.labelLarge?.color ?? ColorRes.disabledGrey;
+Color scaffoldBackgroundColor(BuildContext context) => Theme.of(context).scaffoldBackgroundColor ?? ColorRes.whitePure;
+Color blueFollow(BuildContext context) => Theme.of(context).cardTheme.color ?? ColorRes.blueFollow;
+Color bgMediumGrey(BuildContext context) => Theme.of(context).cardColor ?? ColorRes.bgMediumGrey;
+Color blackPure(BuildContext context) => Theme.of(context).primaryColorDark ?? ColorRes.blackPure;
+Color bgLightGrey(BuildContext context) => Theme.of(context).appBarTheme.backgroundColor ?? ColorRes.bgLightGrey;
+Color themeColor(BuildContext context) => Theme.of(context).canvasColor ?? ColorRes.themeColor;
