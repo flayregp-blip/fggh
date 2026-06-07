74de0370c67319f0c616333055407509f20364f4
flayregp-blip <flayregp@gmail.com>
2026-06-07 13:18:16 +0300

    Clean up theme_res.dart and remove recursive bugs

--- lib/utilities/theme_res.dart
+++ lib/utilities/theme_res.dart
@@ -3,22 +3,15 @@ import 'package:shortzz/utilities/color_res.dart';
 import 'package:shortzz/utilities/font_res.dart';
 
 class ThemeRes {
-  /// Theme light mode
-
   static ThemeData lightTheme(BuildContext context) {
     return ThemeData(
       scaffoldBackgroundColor: ColorRes.whitePure,
       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: ColorRes.whitePure),
+        backgroundColor: ColorRes.whitePure,
+      ),
       appBarTheme: const AppBarTheme(backgroundColor: ColorRes.bgLightGrey),
       fontFamily: FontRes.outFitRegular400,
-      bottomSheetTheme:
-          const BottomSheetThemeData(backgroundColor: ColorRes.whitePure),
-      sliderTheme: const SliderThemeData(
-          trackHeight: 2.5,
-          trackShape: RectangularSliderTrackShape(),
-          overlayShape: RoundSliderOverlayShape(overlayRadius: 0),
-          overlayColor: Colors.transparent),
+      bottomSheetTheme: const BottomSheetThemeData(backgroundColor: ColorRes.whitePure),
       textTheme: const TextTheme(
         titleLarge: TextStyle(color: ColorRes.whitePure),
         titleMedium: TextStyle(color: ColorRes.textDarkGrey),
@@ -26,8 +19,7 @@ class ThemeRes {
         labelSmall: TextStyle(color: ColorRes.themeAccentSolid),
         labelLarge: TextStyle(color: ColorRes.disabledGrey),
       ),
-      textSelectionTheme:
-          const TextSelectionThemeData(selectionColor: ColorRes.disabledGrey),
+      textSelectionTheme: const TextSelectionThemeData(selectionColor: ColorRes.disabledGrey),
       cardTheme: const CardThemeData(color: ColorRes.blueFollow),
       splashColor: Colors.transparent,
       highlightColor: Colors.transparent,
@@ -40,53 +32,55 @@ class ThemeRes {
     );
   }
 
-  /// Theme dark mode
-
   static ThemeData darkTheme(BuildContext context) {
     return ThemeData();
   }
 }
 
+// Helper functions
 Color whitePure(BuildContext context) {
   return Theme.of(context).textTheme.titleLarge?.color ?? ColorRes.whitePure;
 }
 
 Color textDarkGrey(BuildContext context) {
-  return Theme.of(context).textTheme.titleMedium?.color ??
-      textDarkGrey(context);
+  return Theme.of(context).textTheme.titleMedium?.color ?? ColorRes.textDarkGrey;
 }
 
 Color textLightGrey(BuildContext context) {
-  return Theme.of(context).textTheme.titleSmall?.color ??
-      ColorRes.textLightGrey;
+  return Theme.of(context).textTheme.titleSmall?.color ?? ColorRes.textLightGrey;
 }
 
 Color bgGrey(BuildContext context) {
-  return Theme.of(context).dividerColor;
+  return Theme.of(context).dividerColor ?? ColorRes.bgGrey;
+}
 
 Color themeAccentSolid(BuildContext context) {
-  return Theme.of(context).textTheme.labelSmall?.color ??
-      themeAccentSolid(context);
+  return Theme.of(context).textTheme.labelSmall?.color ?? ColorRes.themeAccentSolid;
 }
 
 Color disableGrey(BuildContext context) {
   return Theme.of(context).textTheme.labelLarge?.color ?? ColorRes.disabledGrey;
+}
 
 Color scaffoldBackgroundColor(BuildContext context) {
-  return Theme.of(context).scaffoldBackgroundColor;
+  return Theme.of(context).scaffoldBackgroundColor ?? ColorRes.whitePure;
+}
 
 Color blueFollow(BuildContext context) {
-  return Theme.of(context).cardTheme.color ?? blueFollow(context);
+  return Theme.of(context).cardTheme.color ?? ColorRes.blueFollow;
+}
 
 Color bgMediumGrey(BuildContext context) {
-  return Theme.of(context).cardColor;
+  return Theme.of(context).cardColor ?? ColorRes.bgMediumGrey;
+}
 
 Color blackPure(BuildContext context) {
-  return Theme.of(context).primaryColorDark;
+  return Theme.of(context).primaryColorDark ?? ColorRes.blackPure;
+}
 
 Color bgLightGrey(BuildContext context) {
   return Theme.of(context).appBarTheme.backgroundColor ?? ColorRes.bgLightGrey;
 
 Color themeColor(BuildContext context) {
-  return Theme.of(context).canvasColor;
+  return Theme.of(context).canvasColor ?? ColorRes.themeColor;
 }
